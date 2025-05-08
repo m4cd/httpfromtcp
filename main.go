56c078e79719e0b70c77d2b57a678c2d3f46cf21
m4cd <maciej.ziebicki@outlook.com>
2025-05-08 18:56:57 +0200

    CH1:L5 - Channel Refactor

--- main.go
+++ main.go
@@ -13,23 +13,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	s := ""
-	for {
-		b := make([]byte, 8)
-		_, err = f.Read(b)
-		
-		parts := strings.Split(string(b),"\n")
-		
-		s = s + parts[0]
-		
-		if len(parts) == 2 {
-			fmt.Printf("read: %s\n", s)
-			s = parts[1]
-		}
+	
+	channel := getLinesChannel(f)
 
-		if err == io.EOF {
-			fmt.Printf("read: %s\n", s)
-			break
-		}
+	for line := range channel {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	channel := make(chan string)
+
+	go func(){
+		s := ""
+		for {
+			b := make([]byte, 8)
+			_, err := f.Read(b)
+			
+			parts := strings.Split(string(b),"\n")
+			
+			s = s + parts[0]
+			
+			if len(parts) == 2 {
+				channel <- s
+				s = parts[1]
+			}
+
+			if err == io.EOF {
+				channel <- s
+				break
+			}
+		}
+		defer close(channel)
+	}()
+	return channel
 }
